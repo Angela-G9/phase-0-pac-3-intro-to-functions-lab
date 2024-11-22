c3f9c6ca9211f411634e167175760d4632ec1368
Angela-G9 <angelagathoni9568@gmail.com>
2024-11-22 22:18:00 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+function shout(string) {
+    return string.toUpperCase();
+    
+  }
+  shout('hello'); // Returns 'HELLO'
+
+function whisper(string) {
+    return string.toLowerCase();
+    //whisper ("hello");
+
+}
+whisper ("hello");
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+
+}
+logWhisper('ANGELA');
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+logShout('hello'); // Returns 'HELLO'
+
+function sayHiToHeadphonedRoommate(string){
+    
+
+    if(string === string.toLowerCase()) {
+        return "I can't hear you!";
+    }
+    if(string === string.toUpperCase()) {
+        return "YES INDEED!";
+    }
+    if(string === string.toLowerCase()) {
+        return "I can't hear you";
+    }
+    return;
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    }
+    if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    }
+    if (string === "Let's have dinner together!") {
+      return "I would love to!";
+    }
+    return;
+  }
